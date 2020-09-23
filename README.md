173b84613d883b86990a73fe821c7eefb02eed3e
awantech-io <hello.wanlab@gmail.com>
2020-09-24 00:58:48 +0800

    style button & form

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 ## Facebook Messenger Clone App.
 ## Fullstack build with React, Firebase & Material-UI.
+
+### Checklist
+* [x] Project setup.
+* [x] Send Messages.
+* [x] Retrive & Display Messages.
+* [x] Set layout style.
+    * [x] Install Material-UI.
+    * [x] Style the button.
+    * [x] Style the form.
+    * [x] Style the messages list. 
+
--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Button, FormControl, InputLabel, Input } from '@material-ui/core';
 import './App.css';
 
 function App() {
@@ -10,21 +11,26 @@ function App() {
   console.log(input);
   console.log(messages);
 
-  // function send message inpu
+  // function send message input
   const sendMessage = (event) => {
     // add the input in message array 
     setMessage([...messages, input]);
     setInput('');
+    event.preventDefault();
   }
 
   return (
     <div className="App">
       <h1>Messenger</h1>
 
-      <input value={input} onChange={event => setInput(event.target.value)}/>
-      <button onClick={sendMessage}>Send Message</button>
-      
-      {/* messages  */}
+      <form>
+      <FormControl>
+      <InputLabel >Enter a message...</InputLabel>
+      <Input value={input} onChange={event => setInput(event.target.value)} />
+      <Button disabled={!input} variant="contained" color="primary" type='submit' onClick={sendMessage}>Send Message</Button>
+      </FormControl>    
+      </form>
+
       {
         messages.map(message => (
         <p>{message}</p>
