6b63538866b37dfa340a5e3b78a021ed834c9f76
awantech-io <hello.wanlab@gmail.com>
2020-09-23 23:47:50 +0800

    send and display message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  // declare form input state (using react hooks)
+  const [input, setInput] = useState('');
+  const [messages, setMessage] = useState([]);
+
+  console.log(input);
+  console.log(messages);
+
+  // function send message inpu
+  const sendMessage = (event) => {
+    // add the input in message array 
+    setMessage([...messages, input]);
+    setInput('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Messenger</h1>
+
+      <input value={input} onChange={event => setInput(event.target.value)}/>
+      <button onClick={sendMessage}>Send Message</button>
+      
+      {/* messages  */}
+      {
+        messages.map(message => (
+        <p>{message}</p>
+        ))
+      }
     </div>
   );
 }
